f75b4315c7d03ff22b46e5af942f2dd838e23d2f
erdogansemanur <semanurerdogan1967@gmail.com>
2026-05-12 20:45:35 +0300

    Oyun secme ekrani eklendi

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace TiyatroBiletSistemi
+{
+    partial class Oyunlar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Oyunlar));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelFiltre = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbSehir = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbIlce = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtTarih = new System.Windows.Forms.DateTimePicker();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.flowOyunlar = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panelFiltre.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkRed;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.MaximumSize = new System.Drawing.Size(0, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1182, 80);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(98, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tiyatro Bileti Sistemi";
+            // 
+            // panelFiltre
+            // 
+            this.panelFiltre.BackColor = System.Drawing.Color.Firebrick;
+            this.panelFiltre.Controls.Add(this.btnListele);
+            this.panelFiltre.Controls.Add(this.dtTarih);
+            this.panelFiltre.Controls.Add(this.label4);
+            this.panelFiltre.Controls.Add(this.cmbIlce);
+            this.panelFiltre.Controls.Add(this.label3);
+            this.panelFiltre.Controls.Add(this.cmbSehir);
+            this.panelFiltre.Controls.Add(this.label2);
+            this.panelFiltre.Location = new System.Drawing.Point(18, 100);
+            this.panelFiltre.Name = "panelFiltre";
+            this.panelFiltre.Size = new System.Drawing.Size(250, 541);
+            this.panelFiltre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(16, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Şehir";
+            // 
+            // cmbSehir
+            // 
+            this.cmbSehir.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSehir.FormattingEnabled = true;
+            this.cmbSehir.Location = new System.Drawing.Point(20, 40);
+            this.cmbSehir.Name = "cmbSehir";
+            this.cmbSehir.Size = new System.Drawing.Size(180, 24);
+            this.cmbSehir.TabIndex = 1;
+            this.cmbSehir.SelectedIndexChanged += new System.EventHandler(this.cmbSehir_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(20, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "İlçe";
+            // 
+            // cmbIlce
+            // 
+            this.cmbIlce.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbIlce.FormattingEnabled = true;
+            this.cmbIlce.Location = new System.Drawing.Point(24, 110);
+            this.cmbIlce.Name = "cmbIlce";
+            this.cmbIlce.Size = new System.Drawing.Size(180, 24);
+            this.cmbIlce.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(24, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tarih";
+            // 
+            // dtTarih
+            // 
+            this.dtTarih.Location = new System.Drawing.Point(24, 186);
+            this.dtTarih.Name = "dtTarih";
+            this.dtTarih.Size = new System.Drawing.Size(180, 22);
+            this.dtTarih.TabIndex = 5;
+            // 
+            // btnListele
+            // 
+            this.btnListele.BackColor = System.Drawing.Color.White;
+            this.btnListele.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListele.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnListele.Location = new System.Drawing.Point(24, 230);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(180, 40);
+            this.btnListele.TabIndex = 6;
+            this.btnListele.Text = "Oyunları Listele";
+            this.btnListele.UseVisualStyleBackColor = false;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // flowOyunlar
+            // 
+            this.flowOyunlar.AutoScroll = true;
+            this.flowOyunlar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.flowOyunlar.Location = new System.Drawing.Point(274, 100);
+            this.flowOyunlar.Name = "flowOyunlar";
+            this.flowOyunlar.Size = new System.Drawing.Size(896, 541);
+            this.flowOyunlar.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(18, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Oyunlar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 653);
+            this.Controls.Add(this.flowOyunlar);
+            this.Controls.Add(this.panelFiltre);
+            this.Controls.Add(this.panel1);
+            this.Name = "Oyunlar";
+            this.Text = "Oyunlar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panelFiltre.ResumeLayout(false);
+            this.panelFiltre.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelFiltre;
+        private System.Windows.Forms.ComboBox cmbSehir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbIlce;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.DateTimePicker dtTarih;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FlowLayoutPanel flowOyunlar;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TiyatroBiletSistemi
+{
+    public partial class Oyunlar : Form
+    {
+        Dictionary<string, List<string>> sehirler =
+        new Dictionary<string, List<string>>()
+        {
+            {"Bursa", new List<string>{"Nilüfer","Osmangazi","Yıldırım"}},
+            {"İstanbul", new List<string>{"Kadıköy","Beşiktaş","Üsküdar"}},
+            {"Ankara", new List<string>{"Çankaya","Keçiören","Mamak"}}
+        };
+        class Oyun
+        {
+            public string Ad { get; set; }
+            public string Salon { get; set; }
+            public string Saat { get; set; }
+            public string Sehir { get; set; }
+            public string Poster { get; set; }
+        }
+        List<Oyun> oyunlar = new List<Oyun>()
+        {
+            new Oyun
+            {
+                Ad="Hamlet",
+                Salon="FSM Sahnesi",
+                Saat="20:00",
+                Sehir="Bursa",
+                Poster="hamlet.jpg"
+            },
+
+            new Oyun
+            {
+                Ad="Macbeth",
+                Salon="Konak Sahnesi",
+                Saat="19:30",
+                Sehir="İzmir",
+                Poster="macbeth.jpg"
+            },
+
+            new Oyun
+            {
+                Ad="Romeo ve Juliet",
+                Salon="Kadıköy Sahnesi",
+                Saat="21:00",
+                Sehir="İstanbul",
+                Poster="romeo.jpg"
+            },
+
+            new Oyun
+            {
+                Ad="Othello",
+                Salon="Çankaya Tiyatro",
+                Saat="18:00",
+                Sehir="Ankara",
+                Poster="othello.jpg"
+            }
+        };
+
+
+
+
+        public Oyunlar()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbSehir.Items.Add("Bursa");
+            cmbSehir.Items.Add("İstanbul");
+            cmbSehir.Items.Add("Ankara");
+        }
+
+        private void cmbSehir_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbIlce.Items.Clear();
+
+            string secilenSehir = cmbSehir.SelectedItem.ToString();
+
+            foreach (var ilce in sehirler[secilenSehir])
+            {
+                cmbIlce.Items.Add(ilce);
+            }
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            flowOyunlar.Controls.Clear();
+
+            string secilenSehir = cmbSehir.SelectedItem.ToString();
+
+            var filtreliOyunlar = oyunlar.Where(x => x.Sehir == secilenSehir).ToList();
+
+            foreach (var oyun in filtreliOyunlar)
+            {
+                Panel kart = new Panel();
+                kart.Size = new Size(220, 320);
+                kart.BackColor = Color.White;
+                kart.BorderStyle = BorderStyle.FixedSingle;
+                kart.Margin = new Padding(20);
+
+                PictureBox poster = new PictureBox();
+                poster.Size = new Size(180, 180);
+                poster.Location = new Point(20, 10);
+                poster.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                poster.Image = Image.FromFile(oyun.Poster);
+
+                Label lblAd = new Label();
+                lblAd.Text = oyun.Ad;
+                lblAd.Font = new Font("Arial", 12, FontStyle.Bold);
+                lblAd.Location = new Point(20, 200);
+                lblAd.AutoSize = true;
+
+                Label lblSalon = new Label();
+                lblSalon.Text = oyun.Salon;
+                lblSalon.Location = new Point(20, 230);
+                lblSalon.AutoSize = true;
+
+                Label lblSaat = new Label();
+                lblSaat.Text = oyun.Saat;
+                lblSaat.Location = new Point(20, 255);
+                lblSaat.AutoSize = true;
+
+                Button btnBilet = new Button();
+                btnBilet.Text = "Bilet Al";
+                btnBilet.Size = new Size(100, 35);
+                btnBilet.Location = new Point(55, 280);
+                btnBilet.BackColor = Color.Firebrick;
+                btnBilet.ForeColor = Color.White;
+                btnBilet.FlatStyle = FlatStyle.Flat;
+
+                kart.Controls.Add(poster);
+                kart.Controls.Add(lblAd);
+                kart.Controls.Add(lblSalon);
+                kart.Controls.Add(lblSaat);
+                kart.Controls.Add(btnBilet);
+
+                flowOyunlar.Controls.Add(kart);
+            }
+        }
+    }
+}
