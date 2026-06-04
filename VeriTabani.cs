f6baeeb8e0f3aacb5ee8f51fccec9cefde744290
Neslihan Özdemir <neslihanozdmr.4871@gmail.com>
2026-06-04 20:36:57 +0300

    Güncellendi
    Updated connection string for database access.

--- VeriTabani.cs
+++ VeriTabani.cs
@@ -8,11 +8,10 @@ namespace TiyatroBiletSistemi
    
         public static class VeriTabani
         {
-            // Bağlantı bilgilerin (XAMPP/WampServer kullanıyorsan genelde şifre boştur)
-            // Eğer MySQL için şifre belirlediysen Pwd= kısmına onu yazmalısın.
+            
             private static string connectionString = "Server=localhost;Database=TiyatroDB;Uid=root;Pwd=nesli4345oZ.;";
 
-            // Bu metot projedeki diğer formlardan veri tabanına kolayca bağlanmanı sağlayacak
+            
             public static MySqlConnection BaglantiAl()
             {
                 return new MySqlConnection(connectionString);
